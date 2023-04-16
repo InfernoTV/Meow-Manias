c77f67754314b5c096a15fa73ce6438cde984a67
Inferno <46422592+InfernoTV@users.noreply.github.com>
2023-04-16 14:02:46 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,43 +1,37 @@
-const catContainer = document.getElementById('cat-container');
+// script.js
 
-// Function to fetch cat images from an API
-async function fetchCatImages() {
-  try {
-    const response = await fetch('https://api.thecatapi.com/v1/images/search?limit=20');
-    const data = await response.json();
-    return data;
-  } catch (error) {
-    console.error('Error fetching cat images:', error);
-    return [];
-  }
-}
-
-// Function to create cat cards and append to the container
-function createCatCards(catImages) {
-  catImages.forEach(catImage => {
-    const catCard = document.createElement('div');
-    catCard.className = 'cat-card';
-
-    const catImageElement = document.createElement('img');
-    catImageElement.className = 'cat-image';
-    catImageElement.src = catImage.url;
-    catImageElement.alt = 'Cat';
-
-    catCard.appendChild(catImageElement);
-    catContainer.appendChild(catCard);
+// Function to fetch cat images from the API
+function fetchCatImages() {
+  $.ajax({
+    url: 'https://api.thecatapi.com/v1/images/search?limit=10', // API endpoint for fetching cat images
+    success: function (data) {
+      displayCatImages(data); // Call function to display fetched cat images
+    },
+    error: function () {
+      console.error('Error fetching cat images from API'); // Display error message if fetching fails
+    }
   });
 }
 
-// Function to handle scroll event and fetch more cat images
-async function handleScroll() {
-  if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
-    const catImages = await fetchCatImages();
-    createCatCards(catImages);
-  }
+// Function to display fetched cat images
+function displayCatImages(catImages) {
+  var catContainer = $('#cat-container'); // Get the cat container element
+  catImages.forEach(function (catImage) {
+    // Create a new cat card element
+    var catCard = $('<div>').addClass('cat-card');
+    var catImg = $('<img>').attr('src', catImage.url).attr('alt', 'Cat Image');
+    catCard.append(catImg);
+    catContainer.append(catCard);
+  });
 }
 
-// Event listener for scroll event
-window.addEventListener('scroll', handleScroll);
+// Call the fetchCatImages function to initially fetch and display cat images
+fetchCatImages();
 
-// Initial fetch and rendering of cat images
-fetchCatImages().then(createCatCards);
+// Implement infinite scrolling
+$(window).on('scroll', function () {
+  if ($(window).scrollTop() + $(window).height() >= $(document).height() - 100) {
+    // Fetch and display more cat images when scrolled to the bottom of the page
+    fetchCatImages();
+  }
+});
